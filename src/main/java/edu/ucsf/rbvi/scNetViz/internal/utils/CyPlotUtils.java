7a9dbb39c509de2865083c9ff2125870cd35d627
Scooter Morris <scooter@cgl.ucsf.edu>
2019-01-06 17:34:02 -0800

    Add support for tabIDs

--- src/main/java/edu/ucsf/rbvi/scNetViz/internal/utils/CyPlotUtils.java
+++ src/main/java/edu/ucsf/rbvi/scNetViz/internal/utils/CyPlotUtils.java
@@ -22,7 +22,7 @@ public class CyPlotUtils {
 		argMap.put("title",title);
 		argMap.put("names",names);
 		argMap.put("groups",groups);
-		argMap.put("id",accession+" Violin");
+		argMap.put("id",accession);
 		argMap.put("selectionString","scnetviz select accession=\""+accession+"\" genes=%s");
 		manager.executeCommand("cyplot", "violin", argMap);
 	}
@@ -38,7 +38,7 @@ public class CyPlotUtils {
 		argMap.put("xLabel",xlabel);
 		argMap.put("yLabel",ylabel);
 		argMap.put("title",title);
-		argMap.put("id",accession+" Heatmap");
+		argMap.put("id",accession);
 		argMap.put("selectionString","scnetviz select accession=\""+accession+"\" genes=%s");
 		manager.executeCommand("cyplot", "heat", argMap);
 	}
